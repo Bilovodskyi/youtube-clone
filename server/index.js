a12503074f12a987a45591dcbf026da087fce5e6
Bogdan Bilovodskyi <bogdanbilovodskyi@MacBook-Air-Bogdan.local>
2023-10-20 13:54:33 -0700

    allow methods

--- server/index.js
+++ server/index.js
@@ -33,10 +33,11 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Origin",
         "https://youclone-project.com"
     );
-    res.header(
+    res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     next();
 });
 
