e14c3ddf4e7723c27d299f3ed09713336233c090
Bogdan Bilovodskyi <bogdanbilovodskyi@MacBook-Air-Bogdan.local>
2024-04-01 20:00:28 -0700

    methods plus headers

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
