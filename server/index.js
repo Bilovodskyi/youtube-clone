a050e4f1a635c8cc46e108b20d373814c11e2499
Bogdan Bilovodskyi <bogdanbilovodskyi@MacBook-Air-Bogdan.local>
2024-04-01 20:18:37 -0700

    final

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+app.use(cors());
 
 const connect = () => {
     mongoose
